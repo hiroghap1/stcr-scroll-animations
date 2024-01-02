f3288a25fa124f515ed73f67751c5fb0f1cd26a6
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2024-01-02 20:04:24 +0900

    feat():スタイル読み込み

--- sc-scroll-animations.php
+++ sc-scroll-animations.php
@@ -1,19 +1,24 @@
 <?php
 /*
 Plugin Name: SC Scrolls Animations
-Description: GitHub Actions を実行するボタンを設置します
+Description: スクロールアニメーションを簡単に設定します
 Version: 0.1.0
 Author: HASEGAWA Yoshihiro in Stella Create Inc.
 License: GPLv2
 */
 
-// ファイルを管理画面にロード
-function load_sc_scroll_animations_script() {
-    // メニューページ 'github-actions' でのみスクリプトをロード
-    if (isset($_GET['page']) && $_GET['page'] === 'github-actions') {
-        wp_enqueue_style('sc-scroll-animations-style', plugin_dir_url(__FILE__) . 'css/sc-scroll-animations-style.css');
-        wp_enqueue_script('sc-scroll-animations-script', plugin_dir_url(__FILE__) . 'js/sc-scroll-animations-script.js', array(), null, true);
-    }
+add_action('wp_enqueue_scripts', 'add_styles');
+function add_styles() {
+    wp_enqueue_style('sc-scroll-animations-style', plugin_dir_url(__FILE__) . 'css/sc-scroll-animations-style.css');
 }
-add_action('admin_enqueue_scripts', 'load_sc_scroll_animations_script');
+function load_sc_scroll_animations_script() {
+    wp_enqueue_script('sc-scroll-animations-script', plugin_dir_url(__FILE__) . 'js/sc-scroll-animations-script.js', array(), null, array( 'strategy' => 'defer' ));
+}
+add_action( 'enqueue_block_editor_assets', 'load_sc_scroll_animations_script' );
 
+add_action(
+    'enqueue_block_editor_assets',
+    function() {
+        wp_enqueue_style('sc-scroll-animations-style', plugin_dir_url(__FILE__) . 'css/sc-scroll-animations-style.css');
+    }
+);
